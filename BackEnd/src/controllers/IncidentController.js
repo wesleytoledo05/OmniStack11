3cb1def90dc4a09ba3ab42c8c17bfc4270ce912c
JOSE WESLEY <vntoledo05@gmail.com>
2021-01-19 16:53:31 -0300

    aplicaçao finalizada

--- BackEnd/src/controllers/IncidentController.js
+++ BackEnd/src/controllers/IncidentController.js
@@ -1,6 +1,4 @@
 const connection = require("../database/connection");
-const { index } = require("./OngController");
-const { where, limit } = require("../database/connection");
 
 module.exports = {
   async index(request, response) {
--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -7,14 +7,16 @@ const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
-routes.post("/session", SessionController.create);
+routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
 
-routes.get("/profile", ProfileController.index);
-
 routes.get("/incidents", IncidentController.index);
+routes.get("/incidents/:id", IncidentController.show);
 routes.post("/incidents", IncidentController.create);
 routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.get("/profile", ProfileController.index);
+
 module.exports = routes;
